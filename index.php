30487ec776ba267ee7b50aeef2608c3ff7ec4f92
Rasmus Fuhse <fuhse@data-quest.de>
2015-08-12 08:23:39 +0200

    start

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Week of Decay</title>
+        <link href='http://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300' rel='stylesheet' type='text/css'>
+        <style>
+            body { font-family: 'Open Sans Condensed', sans-serif;}
+            h1 { font-family: 'Montserrat', sans-serif; }
+        </style>
+    </head>
+    <body>
+        <h1>Week Of Decay</h1>
+    </body>
+</html>
