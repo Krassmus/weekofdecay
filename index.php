a218765ecefebf13be4fe98c3f52135223df9b15
Rasmus Fuhse <krassmus@gmail.com>
2015-08-23 20:47:08 +0200

    bugfixing

--- index.php
+++ index.php
@@ -10,10 +10,18 @@ require_once 'lib/Parsedown.php';
         <link href='http://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
         <link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300' rel='stylesheet' type='text/css'>
         <style>
-            body { font-family: 'Open Sans Condensed', sans-serif; background-color: black; }
+            body { font-family: 'Open Sans Condensed', sans-serif; background-color: black; color: grey; }
             h1 { font-family: 'Montserrat', sans-serif; letter-spacing: 0.7em; color: yellow; text-align: center; }
             a { color: #999933; text-decoration: none; }
             a:hover { color: #cccc33; }
+            ul.events {
+                list-style-type: none;
+                max-width: 400px;
+                margin-left: auto;
+                margin-right: auto;
+                padding: 0px;
+                margin-top: 100px;
+            }
             ul.events > li {
                 border-bottom: thin solid lightgrey;
                 border-top: thin solid lightgrey;
@@ -23,7 +31,7 @@ require_once 'lib/Parsedown.php';
     <body>
 	<h1>WEEK OF DECAY</h1>
 
-    <ul style="list-style-type: none; max-width: 400px; margin-left: auto; margin-right: auto;">
+    <ul class="events">
         <?
 	    $postings = file_get_contents("https://pod.geraspora.de/people/edd464d023bc013370b64860008dbc6c/stream.json");
         $postings = json_decode($postings, true);
