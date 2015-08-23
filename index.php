827200cc1ad464676b4297b809cc14a706f0fb83
Rasmus Fuhse <krassmus@gmail.com>
2015-08-23 20:43:41 +0200

    bugfixing

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ require_once 'lib/Parsedown.php';
         <link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300' rel='stylesheet' type='text/css'>
         <style>
             body { font-family: 'Open Sans Condensed', sans-serif; background-color: black; }
-            h1 { font-family: 'Montserrat', sans-serif; letter-spacing: 0.7em; }
+            h1 { font-family: 'Montserrat', sans-serif; letter-spacing: 0.7em; color: yellow; text-align: center; }
             a { color: #999933; text-decoration: none; }
             a:hover { color: #cccc33; }
             ul.events > li {
@@ -23,7 +23,7 @@ require_once 'lib/Parsedown.php';
     <body>
 	<h1>WEEK OF DECAY</h1>
 
-    <ul style="list-style-type: none; max-width: 400px; margin-left: auto;">
+    <ul style="list-style-type: none; max-width: 400px; margin-left: auto; margin-right: auto;">
         <?
 	    $postings = file_get_contents("https://pod.geraspora.de/people/edd464d023bc013370b64860008dbc6c/stream.json");
         $postings = json_decode($postings, true);
